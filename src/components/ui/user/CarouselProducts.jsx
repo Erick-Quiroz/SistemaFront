157324a8fa28bd23022700a971f135c1292c3185
Brand-db <jairo.padilla.deb19@gmail.com>
2023-04-21 03:07:51 -0400

    feat(add): carousel products

---
+++ src/components/ui/user/CarouselProducts.jsx
@@ -0,0 +1,30 @@
+import { Carousel as Carousels } from 'antd'
+import { CardComponent } from './CardComponent'
+import { useEffect, useState } from 'react'
+import { shopAPI } from '../../../services'
+
+export const CarouselProducts = () => {
+    const [products, setProducts] = useState([])
+
+    const getAll = async () => {
+        const { data: { product } } = await shopAPI.get('/productLG/get-productLG')
+        setProducts(product)
+    }
+
+    useEffect(() => {
+        getAll()
+    }, [])
+
+    return (
+        <>
+            <Carousels Carousels autoplay draggable >
+                {
+                    products?.map(product => (
+                        <CardComponent key={product._id} {...product} />
+                    ))
+                }
+            </Carousels >
+
+        </>
+    )
+}
--- src/components/ui/user/index.js
+++ src/components/ui/user/index.js
@@ -1,2 +1,4 @@
 export * from './Navbar.jsx'
 export * from './Footer.jsx'
+export * from './CarouselProducts.jsx'
+export * from './CarouselAdds.jsx'
