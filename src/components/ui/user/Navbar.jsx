75ed68569a61de4eb412063d9f9512f17fe94616
Brand-db <jairo.padilla.deb19@gmail.com>
2023-04-26 00:23:29 -0400

    fix: imgAdds & logo

--- src/components/ui/user/Navbar.jsx
+++ src/components/ui/user/Navbar.jsx
@@ -1,37 +1,34 @@
 import { useState } from 'react'
-import './navbar.css'
 import { Link } from 'react-router-dom'
 import { FaBars } from 'react-icons/fa'
 import { ImCross } from 'react-icons/im'
+import { imageLogo } from '../../../helpers/imageAdds'
+import './navbar.css'
+
 export const Navbar = () => {
     const [Mobile, setMobile] = useState(false)
     return (
-        <>
-            <nav className='navbar'>
-                <img
-                    className='logo'
-                    alt="logo"
-                    src="../../public/logo1.png"
-                />
-
-                <ul className={Mobile ? 'nav-links-mobile' : 'nav-links'} onClick={() => setMobile(false)}>
-                    <Link to={'/admin'} className='text-center'>
-                        <button type="summit" className="btn btn-outline-success "
-                            style={{
-                                height: '10hv',
-                                width: 100,
-                                margin: 6
-                            }}
-                        >Ingresar</button>
-                    </Link>
-                </ul>
-                {
-                }
-                <button className='mobile-menu-icon ' onClick={() => setMobile(!Mobile)}>
-                    {Mobile ? <ImCross /> : <FaBars />}
-                </button>
-            </nav>
-        </>
+        <nav className='navbar'>
+            <img
+                className='logo'
+                alt="logo"
+                src={imageLogo}
+            />
+            <ul className={Mobile ? 'nav-links-mobile' : 'nav-links'} onClick={() => setMobile(false)}>
+                <Link to={'/admin'} className='text-center'>
+                    <button type="summit" className="btn btn-outline-success "
+                        style={{
+                            height: '10hv',
+                            width: 100,
+                            margin: 6
+                        }}
+                    >Ingresar</button>
+                </Link>
+            </ul>
+            <button className='mobile-menu-icon ' onClick={() => setMobile(!Mobile)}>
+                {Mobile ? <ImCross /> : <FaBars />}
+            </button>
+        </nav>
     )
 }
 export default Navbar
