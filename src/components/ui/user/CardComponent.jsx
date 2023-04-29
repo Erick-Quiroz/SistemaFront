045a3c762eb86ad8918895fcd9713f49b87d532d
kruv15 <128865603+kruv15@users.noreply.github.com>
2023-04-28 23:40:50 -0400

    HU Mostrar producto (#38)

--- src/components/ui/user/CardComponent.jsx
+++ src/components/ui/user/CardComponent.jsx
@@ -5,28 +5,28 @@ import PropTypes from 'prop-types'
 const { Meta } = Card
 
 export const CardComponent = (props) => {
-    const { name, description, imageUrl } = props
+    const { name, description, imageUrl, price} = props
 
     return (
         <Row
-            span={24}
-            justify="start"
             align={'top'}
             gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}
             style={{
                 padding: 14,
-                minHeight: '84vh',
-                height: '100%',
-                lineHeight: '100vh',
+                minHeight: '75vh',
+                height: '60vh',
+                lineHeight: '60vh',
                 textAlign: 'center'
             }}
         >
-            <Col span={4} className="gutter-row">
+            <Col span={8} className="gutter-row" style={{
+                maxHeight:'60vh'
+            }}>
                 <Card
                     className={styles.card}
                     style={{
                         marginBottom: 50,
-                        marginLeft: 80,
+                        marginLeft: 0,
                         marginTop: 50,
                         width: 200
                     }}
@@ -34,19 +34,25 @@ export const CardComponent = (props) => {
                         <img alt={name} src={imageUrl} />
                     }
                 >
+                    <div>
+                        precio: {price}
+                    </div>
                     <Meta
                         style={{ color: 'yellowgreen' }}
                         title={name}
-                        description={description}
+                        description={description}                       
                     />
                 </Card>
             </Col>
+
+            
         </Row >
     )
 }
 
 CardComponent.propTypes = {
     name: PropTypes.string,
-    description: PropTypes.string,
-    imageUrl: PropTypes.string
+    description: PropTypes.string, 
+    imageUrl: PropTypes.string,
+    price: PropTypes.number
 }
