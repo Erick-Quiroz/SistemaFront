fe9aec82cfcdced9f36213014b330c065181e0a6
unknown <rodritdc26@gmail.com>
2023-04-30 23:56:58 -0400

    cambios

--- src/pages/Stock/StockPage.jsx
+++ src/pages/Stock/StockPage.jsx
@@ -8,7 +8,7 @@ import { AdminLayout } from '../../components/layouts/AdminLayout.jsx'
 import { useSnackbar } from 'notistack'
 import ModalUpdateStock from './ModalUpdateStock.jsx'
 // import ModalUpdateProduct from './ModalUpdateProduct.jsx'
-import { EditOutlined } from '@ant-design/icons'
+
 export const StockPage = () => {
     const { Content } = Layout
     const { token: { colorBgContainer } } = theme.useToken()
@@ -125,8 +125,8 @@ export const StockPage = () => {
                                         <th scope="col">Precio de venta(Bs)</th>
                                         <th scope="col">Utilidad(Bs)</th>
                                         <th scope="col">Fecha de entrega</th>
-                                        <th scope="col">Cantidad recibida</th>
-                                      
+                                        <th scope="col">Cantidad recibida(ud)</th>
+                                        
                                         <th scope="col">Acciones</th>
                                     </tr>
                                 </thead>
@@ -134,29 +134,29 @@ export const StockPage = () => {
                                     {categories?.map((v, index) =>
                                         <>
                                             <tr className="text-center" style={{ backgroundColor: index % 2 === 0 ? '' : '#F0F8FF' }}>
-                                                
+                                                <td><td>{index + 1}</td></td>
                                                 <td>{v.name}</td>
                                                 <td>{v.cost}</td>
                                                 <td>{v.price}</td>
                                                 <td>{((v.price * 100) - (v.cost * 100)) / 100}</td>
                                                 <td>{v.expiration}</td>
                                                 <td>{v.received}</td>
-                                                
+                                               
                                                 <td>
-                                                <button
+                                                    <button
                                                         className="btn btn-primary"
                                                         onClick={() => {
                                                             handleGetProduct(v._id)
                                                         }} style={{
                                                             padding: 2,
-                                                            width: 30,
+                                                            width: 80,
                                                             margin: 2
+
                                                         }}
-                                                        title='Editar'
                                                     >
-                                                        <EditOutlined/>
+                                                        Editar
                                                     </button>
-                                                  
+
                                                 </td>
                                             </tr>
                                         </>
