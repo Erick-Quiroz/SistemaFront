32b0077bccaad91e4d91d878b90b56b1196bdb4e
Erick-Quiroz <93934190+Erick-Quiroz@users.noreply.github.com>
2023-05-27 20:53:07 -0400

    Update FilterPage
    corrección del error

--- src/pages/Filter/FilterPage.jsx
+++ src/pages/Filter/FilterPage.jsx
@@ -12,8 +12,6 @@ export const FilterPage = () => {
     const [products, setProducts] = useState([])// Todos los productos filtrados por categorias
     const location = useLocation()
     const categoria = location.state.data
-  
-
 
     const getAll = async () => {
         try {
@@ -26,89 +24,19 @@ export const FilterPage = () => {
         }
     }
 
-    const filterProduct = async () => {
-        try {   
-            console.log("envio de datos")
-            console.log(radio)//Precio
-            console.log(checked)//Categoria
-            console.log(categoria)    
-            const { data } = await shopAPI.get(`/productLG/filter-Offer-Category-productLG/${radio}/${checked}/${categoria}`)
-            if (data.success) {
-                setProducts(data.products)
-            }
-        } catch(error){
-            console.log(error)           
-        }
-    }
-
-    const handleRadioPrecioChange = (e) => {
-        const value = e.target.value;
-        if (value === radio) {      
-          setRadio('0');
-        } else {
-          setRadio(value);
-        }
-    };
-
-    const handleRadioOfertaChange = (e) => {
-        const value = e.target.value;
-        if (value === checked) {      
-            setChecked('0');
-        } else {
-            setChecked(value);
-        }
-    };
-
     useEffect(() => {
-
         getAll()
     }, [])
-
 
     return (
         <>
             <ShopLayout>
                 <Content>
-
                     <>
                         <Row justify="center">
-
                             <Title className='Titulo'>Busqueda de Productos</Title>
                         </Row>
                         <Row>
-                            <Col span={2} className='Buscador' style={{
-                                minWidth:'100px'
-                            }}>
-                                <div className='Fondo' style={{
-                                    backgroundColor: 'white',
-                                    
-                                }}>
-                                    <Row justify="center">
-                                        <Title level={4}>Precio</Title>                                       
-                                    </Row>
-                                    <Row>
-                                        <Radio.Group defaultValue= {"0"} value={radio}>
-                                            <Space direction="vertical">
-                                                <Radio value="1" onClick={handleRadioPrecioChange}>De menor a mayor</Radio>
-                                                <Radio value="2" onClick={handleRadioPrecioChange}>De mayor a menor</Radio>
-                                            </Space>
-                                        </Radio.Group>
-                                    </Row>
-                                    <Row justify="center">
-                                        <Title level={4}>Ofertas</Title>                                       
-                                    </Row>
-                                    <Row>
-                                        <Title level={5}>Todas las ofertas</Title>                                       
-                                    </Row>
-                                    <Row>
-                                        <Radio.Group value={checked}>
-                                            <Space direction="vertical">
-                                                <Radio value="1" onClick={handleRadioOfertaChange}>Oferta</Radio>
-                                            </Space>
-                                        </Radio.Group>
-                                    </Row>
-                                </div>
-                            </Col>
                             <Col span={22} className='Productos' style={{
                                 flexWrap: 'nowrap'
                             }}>
@@ -121,10 +49,8 @@ export const FilterPage = () => {
                                         ))
                                     }
                                 </Row>
-
                             </Col>
                         </Row>
-
                     </>
                 </Content >
             </ShopLayout>
