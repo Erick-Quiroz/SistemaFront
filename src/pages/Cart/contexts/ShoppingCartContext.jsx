57b2d2c3d16bea1be740737b10c31f58ade90093
Erick-Quiroz <93934190+Erick-Quiroz@users.noreply.github.com>
2023-05-27 20:59:54 -0400

    Update ShoppingCartContext.jsx
    solución error

--- src/pages/Cart/contexts/ShoppingCartContext.jsx
+++ src/pages/Cart/contexts/ShoppingCartContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 export const CartContext = createContext(null)
@@ -6,13 +6,26 @@ export const CartContext = createContext(null)
 export const ShoppingCartProvider = ({ children }) => {
     const [cart, setCart] = useState([])
 
+    // Cargar datos del carrito desde el caché al iniciar la aplicación
+    useEffect(() => {
+        const cachedCart = localStorage.getItem('cart')
+        if (cachedCart) {
+            setCart(JSON.parse(cachedCart))
+        }
+    }, [])
+
+    // Guardar datos del carrito en el caché cuando el carrito cambia
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
     return (
-        <>
-
-            <CartContext.Provider value={[cart, setCart]}>
-
-                {children}
-            </CartContext.Provider>
-        </>
+        <CartContext.Provider value={[cart, setCart]}>
+            {children}
+        </CartContext.Provider>
     )
+}
+
+ShoppingCartProvider.propTypes = {
+    children: PropTypes.node
 }
